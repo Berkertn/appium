59e6cfa75db26a99ff5f590390272088e047848f
Berk Work <berk.ertan@commencis.com>
2023-06-02 00:24:12 +0300

    new Driver using option class

--- src/test/java/Driver/CreateDriverSessionUsingOptions.java
+++ src/test/java/Driver/CreateDriverSessionUsingOptions.java
@@ -4,20 +4,33 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CreateDriverSessionUsingOptions {
     public static void main(String[] args) throws MalformedURLException {
-        Driver driverOptions = new Driver(Driver.PlatformNames.android);
+
+        /*Driver driverOptions = new Driver(Driver.PlatformNames.android);
+
         UiAutomator2Options options = new UiAutomator2Options()
                 .setDeviceName(driverOptions.getDeviceName())
                 .setAutomationName(driverOptions.getAutomationName())
                 .setApp(driverOptions.getAppPath());
 
-        URL url = new URL("http://0.0.0.0:4723"); //after appium 2.0 we don't use /wd/hub
+        URL url = new URL("http://0.0.0.0:4723");
 
-        AppiumDriver driver = new AndroidDriver(url, options);
+        AppiumDriver driver = new AndroidDriver(url, options);*/
+
+        Driver driverOptions = new Driver(Driver.PlatformNames.ios);
+        XCUITestOptions options = new XCUITestOptions()
+                .setDeviceName(driverOptions.getDeviceName())
+                .setAutomationName(driverOptions.getAutomationName())
+                .setApp(driverOptions.getAppPath());
+
+        URL url = new URL("http://0.0.0.0:4723");
+
+        AppiumDriver driver = new IOSDriver(url, options);
     }
 }
