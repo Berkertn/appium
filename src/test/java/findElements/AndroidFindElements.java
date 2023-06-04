ec7f0e8b31fdbfa3fa1a1db3d6bef99fe8fc354b
ertanb <berk.ertan@commencis.com>
2023-06-04 20:47:25 +0300

    android find element learning ex

---
+++ src/test/java/findElements/AndroidFindElements.java
@@ -0,0 +1,22 @@
+package findElements;
+
+import Driver.CreateDriverSessionUsingOptions;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+
+
+import java.net.MalformedURLException;
+
+public class AndroidFindElements {
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        AndroidDriver driver = (AndroidDriver) CreateDriverSessionUsingOptions.initDriver("Android");
+
+        AppiumBy accessibilityTextFieldSelector = (AppiumBy) AppiumBy.accessibilityId("Accessibility");
+        WebElement accessibilityTextFieldElement = driver.findElement(accessibilityTextFieldSelector);
+        System.out.println(accessibilityTextFieldElement.getAttribute("password"));
+        System.out.println(accessibilityTextFieldElement.getText());
+    }
+}
