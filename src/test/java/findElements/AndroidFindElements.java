a6d93790ab25038ee7cb9e79815441b2de6366f2
ertanb <berk.ertan@commencis.com>
2023-06-06 14:09:39 +0300

    gestures swipe left added

--- src/test/java/findElements/AndroidFindElements.java
+++ src/test/java/findElements/AndroidFindElements.java
@@ -8,15 +8,19 @@ import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.net.MalformedURLException;
+import java.time.Duration;
 
 public class AndroidFindElements {
 
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
         AndroidDriver driver = (AndroidDriver) CreateDriverSessionUsingOptions.initDriver("Android");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         System.out.println(driver.getSessionId());
 
 
@@ -27,7 +31,9 @@ public class AndroidFindElements {
         viewsButtonElement.click();
 
         //swipe
-        WebElement element = driver.findElement(AppiumBy.id("android:id/list"));
+        AppiumBy list = (AppiumBy) AppiumBy.id("android:id/list");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(list));
+        WebElement element = driver.findElement(list);
 
         driver.executeScript("mobile: swipeGesture", ImmutableMap.of(
                 "elementId", ((RemoteWebElement) element).getId(),
@@ -39,6 +45,16 @@ public class AndroidFindElements {
         driver.findElement(editText).sendKeys("my text");
         Thread.sleep(3000);
         driver.findElement(editText).clear();
+
+        System.out.println("second swipe");
+
+        //swipe to exit
+        WebElement element2 = driver.findElement(AppiumBy.id("android:id/decor_content_parent"));
+        driver.executeScript("mobile: swipeGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) element2).getId(),
+                "direction", "right",
+                "percent", 0.75
+        ));
 
     }
 }
