46e6bedc669520721f88a2d21222361c56f0ef8c
Berk Ertan <berk.ertan@commencis.com>
2023-06-11 19:01:49 +0300

    long click added

--- src/test/java/findElements/AndroidFindElements.java
+++ src/test/java/findElements/AndroidFindElements.java
@@ -2,59 +2,37 @@ package findElements;
 
 
 import Driver.CreateDriverSessionUsingOptions;
-import com.google.common.collect.ImmutableMap;
+import bases.StepDefinitionBase;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.net.MalformedURLException;
-import java.time.Duration;
 
 public class AndroidFindElements {
 
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
         AndroidDriver driver = (AndroidDriver) CreateDriverSessionUsingOptions.initDriver("Android");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        StepDefinitionBase steps = new StepDefinitionBase(driver);
         System.out.println(driver.getSessionId());
 
 
         By viewsSelector = AppiumBy.accessibilityId("Views");
+
         By textFields = AppiumBy.accessibilityId("TextFields");
-        By editText = AppiumBy.id("io.appium.android.apis:id/edit");
-        WebElement viewsButtonElement = driver.findElement(viewsSelector);
+        WebElement viewsButtonElement = steps.getElement(viewsSelector);
         viewsButtonElement.click();
+        steps.getElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        steps.longClick(By.id("io.appium.android.apis:id/drag_dot_1"));
 
         //swipe
+        /*
         AppiumBy list = (AppiumBy) AppiumBy.id("android:id/list");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(list));
-        WebElement element = driver.findElement(list);
+        steps.swipeTo(list,"up");*/
 
-        driver.executeScript("mobile: swipeGesture", ImmutableMap.of(
-                "elementId", ((RemoteWebElement) element).getId(),
-                "direction", "up",
-                "percent", 0.75
-        ));
-
-        driver.findElement(textFields).click();
-        driver.findElement(editText).sendKeys("my text");
-        Thread.sleep(3000);
-        driver.findElement(editText).clear();
-
-        System.out.println("second swipe");
-
-        //swipe to exit
-        WebElement element2 = driver.findElement(AppiumBy.id("android:id/decor_content_parent"));
-        driver.executeScript("mobile: swipeGesture", ImmutableMap.of(
-                "elementId", ((RemoteWebElement) element2).getId(),
-                "direction", "right",
-                "percent", 0.75
-        ));
 
     }
 }
